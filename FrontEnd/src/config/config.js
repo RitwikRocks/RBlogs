a387177e2f955ed9e8187636c50d2b17e5fa548d
RitwikRocks <ritwikrocks26@gmail.com>
2024-07-17 19:42:59 +0530

    Production Change 11

--- FrontEnd/src/config/config.js
+++ FrontEnd/src/config/config.js
@@ -5,7 +5,7 @@ const config =
         appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
         appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
         appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-        appwriteTinyMCEApiKey:String(import.meta.env.VITE_TINY_MCE_API_KEY)
+        appwriteTinyMCEApiKey:String(import.meta.env.VITE_TINY_MCE_API)
     }
 
 export default config;
